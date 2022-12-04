cab7c27a6684b7e6c182e59f85587d14085a0f44
OzBirdog <116414879+OzBirdog@users.noreply.github.com>
2022-12-03 20:34:21 -0600

    Update requestUnicorn.js

--- js/requestUnicorn.js
+++ js/requestUnicorn.js
@@ -10,27 +10,27 @@ const ddb = new AWS.DynamoDB.DocumentClient();
 
 const fleet = [
 	{
-		Name: 'Bucephalus',
+		Name: 'Oz Birdett 1',
 		Color: 'Golden',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Scrappy',
+		Name: 'Oz Birdett 2',
 		Color: 'Mean Green',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Cosmo',
+		Name: 'Oz Birdett 4',
 		Color: 'Royal Blue',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Shadowfax',
+		Name: 'Oz Birdett 5',
 		Color: 'White',
 		Gender: 'Male',
 	},
 	{
-		Name: 'Rocinante',
+		Name: 'Oz Birdett 6',
 		Color: 'Yellow',
 		Gender: 'Female',
 	},
